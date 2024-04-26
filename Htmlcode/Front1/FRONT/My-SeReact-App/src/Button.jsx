41ed2af6e12740245abc9b49d88401e11969c630
RAVI SAIPRIYA <amulupravin@gmail.com.com>
2024-04-26 14:49:22 -0500

    =Htmlcode

---
+++ Htmlcode/Front1/FRONT/My-SeReact-App/src/Button.jsx
@@ -0,0 +1,25 @@
+function Button(){
+    const handleClick = (e) => e.target.textContent = "OUCH!";
+
+
+ //   let count = 0;
+
+//* const handleClick =(name) => {
+//*if(count <3){
+//*    count++;
+//*   console.log(`${name} you clicked me ${count} time/s`);
+//*}else{
+//*    console.log(`${name} stop clicking me!`)
+//*}
+
+
+//*};-->
+
+//    const handleClick2 =(name) => console.log(`${name} stop clicking me`);
+    return(
+        
+        <button onDoubleClick = {(e) => handleClick(e)}> Click me </button> 
+        
+    );
+}
+export default Button
